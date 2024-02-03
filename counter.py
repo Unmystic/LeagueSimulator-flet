e833498367e6ad3e3570d08ee3440189f3ea0658
Dmitriy P <Unmystic7@gmail.com>
2024-02-03 22:38:48 +0300

    Starting developing main screen -- menu

--- counter.py
+++ counter.py
@@ -1,25 +1,32 @@
 import flet as ft
+import time, threading
+
+class Countdown(ft.UserControl):
+    def __init__(self, seconds):
+        super().__init__()
+        self.seconds = seconds
+
+    def did_mount(self):
+        self.running = True
+        self.th = threading.Thread(target=self.update_timer, args=(), daemon=True)
+        self.th.start()
+
+    def will_unmount(self):
+        self.running = False
+
+    def update_timer(self):
+        while self.seconds and self.running:
+            mins, secs = divmod(self.seconds, 60)
+            self.countdown.value = "{:02d}:{:02d}".format(mins, secs)
+            self.update()
+            time.sleep(1)
+            self.seconds -= 1
+
+    def build(self):
+        self.countdown = ft.Text()
+        return self.countdown
 
 def main(page: ft.Page):
-
-    c = ft.Container(
-        width=150,
-        height=150,
-        bgcolor="blue",
-        border_radius=10,
-        animate_opacity=300,
-    )
-
-    def animate_opacity(e):
-        c.opacity = 0 if c.opacity == 1 else 1
-        c.update()
-
-    page.add(
-        c,
-        ft.ElevatedButton(
-            "Animate opacity",
-            on_click=animate_opacity,
-        ),
-    )
+    page.add(Countdown(120), Countdown(60))
 
 ft.app(target=main)
